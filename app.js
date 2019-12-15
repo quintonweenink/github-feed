d37d2772d78dcba99e4eac222a678af5775625a6
Quinton Weenink <quinton.weenink@standardbank.co.za>
2019-12-15 15:38:09 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const CONNECTION_URL = process.env.CONNECTION_URL
+const DATABASE_NAME = process.env.DATABASE_NAME
+
+const Express = require("express")
+const cors = require("cors")
+const BodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
+const ObjectId = require("mongodb").ObjectID
+
+var app = Express()
+app.use(BodyParser.json())
+app.use(BodyParser.urlencoded({ extended: true }))
+app.use(cors())
+var database, collection
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if (error) {
+            throw error
+        }
+        database = client.db(DATABASE_NAME)
+        collection = database.collection("read_later")
+        console.log("Connected to `" + DATABASE_NAME + "`!")
+    })
+})
+
+app.put("/read-later/:username", (request, response) => {
+    const username = request.params.username
+    const items = request.body
+
+    collection.findOne({ username: username }, (error, result) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+        if (result.length < 1) {
+            var insertData = { username: username, items: items }
+
+            collection.insertOne(insertData, (error, result) => {
+                if (error) {
+                    return response.status(500).send(error)
+                }
+                response.send(result.result)
+            })
+        } else {
+            var updateItemsQuery = { $push: { items: { $each: items } } }
+
+            collection.updateOne({ username: username }, updateItemsQuery, (error, result) => {
+                if (error) {
+                    return response.status(500).send(error)
+                }
+                response.send(result.result)
+            })
+        }
+    })
+})
+
+app.delete("/read-later/:username", (request, response) => {
+    const username = request.params.username
+    const items = request.body
+    const updateItemsQuery = { $pull: { items: { $in: items } } }
+
+    collection.updateOne({ username: username }, updateItemsQuery, (error, result) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+        response.send(result.result)
+    })
+})
+
+app.get("/read-later/:username", (request, response) => {
+    const username = request.params.username
+
+    collection.findOne({ username: username }, (error, result) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+        response.send(result)
+    })
+})
+
+app.get("/read-later", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+        response.send(result)
+    })
+})
